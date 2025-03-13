56c1544cf39fcc77c6fffe3d54933e8891d22e8a
davidstrasak <davidstrasak@seznam.cz>
2025-03-13 16:55:52 +0100

    improve the appid of the capacitor app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "net.davidstrasak.app",
+  appId: "net.davidstrasak.conveyorcontroller",
   appName: "Conveyor control",
   webDir: "dist",
 };
